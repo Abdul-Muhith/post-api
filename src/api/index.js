f17969e046c11b97e3a6eb2e8658c62fbf248dc6
Abdul Muhith <amuhith.mern@outlook.com>
2023-10-22 12:51:33 +0600

    api updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 
-const url = 'http://localhost:5000/posts'
+// const url = 'http://localhost:5000/posts'
+const url = 'https://memories-backend-alpha.vercel.app/posts'
 
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
